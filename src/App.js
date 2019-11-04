9ac542ceed14a5f33ac30d17ace901126fbec4dd
Bhavya <Bhavya.Gulati@walmartlabs.com>
2019-11-04 11:22:22 -0800

    Adding onChange event handler

--- src/App.js
+++ src/App.js
@@ -5,14 +5,9 @@ import Person from './Person/Person'
 class App extends Component {
   state = {
     persons: [
-      {
-        name: 'Max',
-        age: 28
-      },
-      {
-        name: 'Manu',
-        age: 29
-      }
+      {name: 'Bhavya', age: 24},
+      {name: 'Aradhya', age: 27},
+      {name: 'Shefali', age: 42}
     ],
     otherState: 'This is other state'
   }
@@ -21,14 +16,20 @@ class App extends Component {
     console.log(this.state);
     // console.log('Yes was clicked!');
     this.setState({
-      persons: [{
-          name: 'Bhavya',
-          age: 24
-        },
-        {
-          name: newName,
-          age: 27
-        }
+      persons: [
+        {name: 'Bhavya', age: 24},
+        {name: newName, age: 27},
+        {name: 'Shefali', age: 42}
+      ]
+    });
+  }
+
+  changeNameHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: 'Bhavya', age: 24},
+        {name: event.target.value, age: 27},
+        {name: 'Shefali', age: 42}
       ]
     });
   }
@@ -40,8 +41,11 @@ class App extends Component {
         <h1>Hi, I am a React App</h1>
         <button onClick={() => this.switchNameHandler('Arun!!')}>Switch name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}><p>My Hobbies: Cooking</p></ Person>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.switchNameHandler.bind(this,'Aradhya!!')}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          changed={this.changeNameHandler}><p>My Hobbies: Cooking</p></ Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} click={this.switchNameHandler.bind(this,'Aradhya!!')}/>
       </div>
     );
   }
