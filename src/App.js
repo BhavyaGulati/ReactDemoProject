cdfc632b69b068fab2b807e2859485609cbcfb4c
Bhavya <Bhavya.Gulati@walmartlabs.com>
2019-11-03 20:45:16 -0800

    Add switchNameHandler for click button

--- src/App.js
+++ src/App.js
@@ -13,15 +13,32 @@ class App extends Component {
         name: 'Manu',
         age: 29
       }
-    ]
+    ],
+    otherState: 'This is another state'
   }
+
+  switchNameHandler = () =>{
+    // console.log('Yes was clicked!');
+    this.setState({
+      persons: [{
+          name: 'Bhavya',
+          age: 24
+        },
+        {
+          name: 'Aradhya',
+          age: 27
+        }
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
-        <button>Switch name</button>
+        <button onClick={this.switchNameHandler}>Switch name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Cooking</ Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}><p>My Hobbies: Cooking</p></ Person>
       </div>
     );
   }
