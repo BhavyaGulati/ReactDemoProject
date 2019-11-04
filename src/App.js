0d1ced0a3d64b4851311458736326e5f3f9b6b8a
Bhavya <Bhavya.Gulati@walmartlabs.com>
2019-11-04 10:33:50 -0800

    Use react hooks - useState

--- src/App.js
+++ src/App.js
@@ -1,47 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
-    persons: [
-      {
-        name: 'Max',
-        age: 28
-      },
-      {
-        name: 'Manu',
-        age: 29
+const App = () => {
+  const [personState, setPerson] = useState(
+    {
+        persons: [{
+            name: 'Max',
+            age: 28
+          },
+          {
+            name: 'Manu',
+            age: 29
+          }
+        ]
       }
-    ],
-    otherState: 'This is another state'
-  }
+  );
 
-  switchNameHandler = () =>{
-    // console.log('Yes was clicked!');
-    this.setState({
-      persons: [{
-          name: 'Bhavya',
-          age: 24
-        },
-        {
-          name: 'Aradhya',
-          age: 27
-        }
-      ]
-    });
-  }
+  const [otherState, setOtherState] = useState('This is another state')
 
-  render() {
-    return (
+  const switchNameHandler = () => {
+        setPerson({
+          persons: [{
+              name: 'Bhavya',
+              age: 24
+            },
+            {
+              name: 'Aradhya',
+              age: 27
+            }
+          ]
+        });
+      };
+
+  return(
       <div className="App">
         <h1>Hi, I am a React App</h1>
-        <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}><p>My Hobbies: Cooking</p></ Person>
+        <button onClick={switchNameHandler}>Switch name</button>
+        <Person name={personState.persons[0].name} age={personState.persons[0].age}/>
+        <Person name={personState.persons[1].name} age={personState.persons[1].age}><p>My Hobbies: Cooking</p></ Person>
       </div>
     );
-  }
-}
+};
 
 export default App;
